29747667f62d4029cb00625b6a47df366abf7095
sudhir <sudhishende@gmail.com>
2017-03-03 11:35:25 +0000

    Add GET /users/me and authentication middleware

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,8 @@
 
 var env = process.env.NODE_ENV || 'development';
 
+
+
 if(env ==='development'){
    process.env.PORT = 3000; 
    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
@@ -8,3 +10,4 @@ if(env ==='development'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
+
